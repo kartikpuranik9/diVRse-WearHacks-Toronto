96c84d7b59fa0052718a3247b8220c3bbb9bd24e
Kartik Karkala <puranikk@Kartiks-MacBook-Pro.local>
2015-05-09 11:27:30 -0400

    Added all lua files

---
+++ myoTest2.js
@@ -0,0 +1,11 @@
+var Myo = require('myo');
+
+var myMyo = Myo.create();
+myMyo.on('fist', function(edge){
+    if(!edge) return;
+    console.log('Clicked!');
+	/*myMyo.mouse('left', 'click')*/
+	/*myMyo.centerMousePosition();*/
+	console.log(myMyo.getArm());
+    /*myMyo.vibrate();*/
+});
